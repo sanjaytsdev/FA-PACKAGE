6fae98cc637ac7896e88c5d0371af6aaf6a009c1
Sanjay T S <sanjaytsdev@gmail.com>
2026-03-13 21:57:13 +0530

    Initial project setup

---
+++ Backend/src/main/java/com/spam/financialaccounting/FinancialaccountingApplication.java
@@ -0,0 +1,13 @@
+package com.spam.financialaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialaccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialaccountingApplication.class, args);
+	}
+
+}
