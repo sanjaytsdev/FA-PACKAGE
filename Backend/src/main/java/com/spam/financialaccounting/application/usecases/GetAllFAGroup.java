bdae5b2f5c36c5f7c493f41f1f788bd617589e05
ashna <ashna@midhaan.com>
2026-03-29 18:46:53 +0530

    Implement getAll FAGroup API

---
+++ Backend/src/main/java/com/spam/financialaccounting/application/usecases/GetAllFAGroup.java
@@ -0,0 +1,21 @@
+package com.spam.financialaccounting.application.usecases;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.spam.financialaccounting.domain.entity.FAGroup;
+import com.spam.financialaccounting.domain.repository.FAGroupRepository;
+
+@Service
+public class GetAllFAGroup {
+    private final FAGroupRepository repository;
+
+    public GetAllFAGroup(FAGroupRepository repository) {
+        this.repository = repository;
+    }
+
+    public  List<FAGroup> execute() {
+        return repository.findAll();
+    }
+}
--- Backend/src/main/java/com/spam/financialaccounting/presentation/controller/FAGroupController.java
+++ Backend/src/main/java/com/spam/financialaccounting/presentation/controller/FAGroupController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import com.spam.financialaccounting.application.usecases.CreateFAGroup;
 import com.spam.financialaccounting.application.usecases.UpdateFAGroup;
+import com.spam.financialaccounting.application.usecases.GetAllFAGroup;
 import com.spam.financialaccounting.domain.entity.FAGroup;
 import com.spam.financialaccounting.infrastructure.persistence.mapper.FAGroupDTOMapper;
 import com.spam.financialaccounting.presentation.dto.FAGroupDTO;
@@ -21,10 +26,12 @@ import jakarta.validation.Valid;
 public class FAGroupController {
     private final CreateFAGroup createUseCase;
     private final UpdateFAGroup updateUseCase;
+    private final GetAllFAGroup getAllUseCase;
 
-    public FAGroupController(CreateFAGroup createUseCase, UpdateFAGroup updateUseCase) {
+    public FAGroupController(CreateFAGroup createUseCase, UpdateFAGroup updateUseCase, GetAllFAGroup getAllUseCase) {
         this.createUseCase = createUseCase;
         this.updateUseCase = updateUseCase;
+        this.getAllUseCase = getAllUseCase;
     }
 
     @PostMapping
@@ -40,4 +47,17 @@ public class FAGroupController {
         FAGroup updatedEntity =updateUseCase.execute(entity);
         return ResponseEntity.ok(FAGroupDTOMapper.toDTO(updatedEntity));
     }
+
+    @GetMapping
+    public ResponseEntity<List<FAGroupDTO>> getAll() {
+        List<FAGroup> entities = getAllUseCase.execute();
+        List<FAGroupDTO> dtos = new ArrayList<>();
+
+        for(FAGroup entity : entities){
+            FAGroupDTO dto = FAGroupDTOMapper.toDTO(entity);
+            dtos.add(dto);
+        }
+        return ResponseEntity.ok(dtos);
+    }
+
 }
