d32528eb46edd2305eb3b24d09e81cee778144aa
Amith Krishnan <amithkrishna29z@gmail.com>
2026-04-13 21:42:48 +0530

    Update GlobalExceptionHandler

---
+++ Backend/src/main/java/com/spam/financialaccounting/presentation/exception/FASubGroupException.java
@@ -0,0 +1,7 @@
+package com.spam.financialaccounting.presentation.exception;
+
+public abstract class FASubGroupException extends RuntimeException {
+    public FASubGroupException(String message) {
+        super(message);
+    }
+}
---
+++ Backend/src/main/java/com/spam/financialaccounting/presentation/exception/FASubGroupValidationException.java
@@ -0,0 +1,7 @@
+package com.spam.financialaccounting.presentation.exception;
+
+public class FASubGroupValidationException extends FASubGroupException {
+    public FASubGroupValidationException(String message) {
+        super(message);
+    }
+}
