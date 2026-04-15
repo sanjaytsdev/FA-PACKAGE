27651b7d89c53cdbdb0e0535fe8deeb00b10ea1b
ashna <ashna@midhaan.com>
2026-04-15 12:39:59 +0530

    create FASubGroupByCode usecase

---
+++ Backend/src/main/java/com/spam/financialaccounting/application/usecases/GetFASubGroupByCode.java
@@ -0,0 +1,26 @@
+package com.spam.financialaccounting.application.usecases;
+
+import org.springframework.stereotype.Service;
+
+import com.spam.financialaccounting.domain.entity.FASubGroup;
+import com.spam.financialaccounting.domain.repository.FASubGroupRepository;
+import com.spam.financialaccounting.presentation.exception.fagroup.FASubGroupNotFoundException; 
+
+@Service
+public class GetFASubGroupByCode {
+    
+    private final FASubGroupRepository repository;
+
+    public GetFASubGroupByCode(FASubGroupRepository repository){
+        this.repository = repository;
+    }
+
+    public FASubGroup execute(String sCode) {
+        if (sCode == null || sCode.trim().isEmpty()) {
+            throw new IllegalArgumentException("sCode cannot be null or empty");
+        }
+        return repository.findByCode(sCode).orElseThrow(() ->new FASubGroupNotFoundException("FASubGroup not found with code: "+sCode));
+    }
+
+
+}
